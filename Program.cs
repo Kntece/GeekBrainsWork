f6af7f3a9745f5aaadfb1b5d26f3950a52322e9d
Kntece <kntece@mail.ru>
2022-08-02 09:20:32 +0300

    Написал файл program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+    Console.WriteLine();
+}
+string[] firstArray = { "Ops", "Work", "Money", "Lex", "))", "+", "12345" };
+int length = 1;
+string[] responseArray = new string[length];
+int index = 0;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if(firstArray[i].Length <= 3)
+    {
+        responseArray[index] = firstArray[i];
+        index ++;
+        length ++;
+        Array.Resize(ref responseArray, length);
+    }
+    
+}
+Console.Write("Первоначальный массив: ");
+PrintArray(firstArray);
+Console.WriteLine();
+Console.Write("Вывод: ");
+PrintArray(responseArray);
